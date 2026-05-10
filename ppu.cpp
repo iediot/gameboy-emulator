ccb2c561f8247a11b94f65bc226bd028dd1aca57
nicolae.tabirca <nicolae.tabirca@stud.acs.upb.ro>
2026-05-10 22:51:20 +0300

    added a bare ppu timing skeleton

--- ppu.cpp
+++ ppu.cpp
@@ -9,10 +9,23 @@ Ppu::Ppu(Memory& memory) : mem(memory) {
 
 void Ppu::step(uint8_t cycles) {
     scanline_cycles += cycles;
+    uint8_t mode = 0;
     if (scanline_cycles >= 456) {
         scanline_cycles -= 456;
-        mem.write(0xFF44, mem.read(0xFF44) + 1);
-        if (mem.read(0xFF44) >= 154)
-            mem.write(0xFF44, 0);
+        mem.write(0xFF44, mem.read(0xFF44) + 1); // LY increment
+        if (mem.read(0xFF44) >= 154) // check LY if over 154
+            mem.write(0xFF44, 0); // reset to 0 if true
+        if (mem.read(0xFF44) == 144)
+            mem.write(0xFF0F, mem.read(0xFF0F) | 0x01);
     }
+    if (mem.read(0xFF44) >= 144) { // mode 1 - VBlank
+        mode = 1;
+    } else if (scanline_cycles < 80) { // mode 2 - OAM scan
+        mode = 2;
+    } else if (scanline_cycles < 252) { // mode 3 - Drawing
+        mode = 3;
+    } else { // mode 0 - HBlank
+        mode = 0;
+    }
+    mem.write(0xFF41, (mem.read(0xFF41) & 0xFC) | mode);
 }
